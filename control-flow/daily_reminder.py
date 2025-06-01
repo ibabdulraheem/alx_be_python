304aafcbed71c03485b16a6448c9d73552153d4f
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-06-01 08:04:06 +0100

    pattern

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,15 +1,15 @@
 task = input("Enter your task: ")
-periority = input("Priority (high/medium/low): ")
+priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
-match periority:
+match priority:
   case "high":
-    if periority == "high" and time_bound == "yes":
-      print (f"Reminder: {task} is a {periority} periority task that requires immediate attention today!")
+    if priority == "high" and time_bound == "yes":
+      print (f"Reminder: {task} is a {priority} periority task that requires immediate attention today!")
   case "medium":
-    if periority == "medium" and time_bound == "yes":
-      print(f"Reminder: {task} is a {periority} periority task that requires your attension when you are less buzy today or later!")
+    if priority == "medium" and time_bound == "yes":
+      print(f"Reminder: {task} is a {priority} periority task that requires your attension when you are less buzy today or later!")
   case "low":
-    if periority == "low" and time_bound == "no":
-      print(f"Note: {task} is a {periority} priority task. Consider completing it when you have free time")
+    if priority == "low" and time_bound == "no":
+      print(f"Note: {task} is a {priority} priority task. Consider completing it when you have free time")
   case _:
     print("The task requires no attesion!")
