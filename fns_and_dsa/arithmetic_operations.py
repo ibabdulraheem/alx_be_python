a83ad380b76cfcad3c5345fa33d2e76271db6e86
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-06-07 22:37:15 +0100

    arithmetic operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -12,7 +12,7 @@ def perform_operation(num1, num2, operation):
     if num2!=0:
       result = num1/num2
     elif num2 == 0:
-      result = "Invalid operation"
+      return "Invalid operation"
   print(f"Result: {result}")
 perform_operation ( 1, 2, "add" )
   
