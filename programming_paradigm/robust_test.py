f9f6a2c5963a2e8d611e3630e4ce62dd8b3cb853
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-06-15 23:00:36 +0100

    robust division

--- programming_paradigm/robust_test.py
+++ programming_paradigm/robust_test.py
@@ -1,7 +1,7 @@
 import sys
 from robust_division_calculator import safe_divide
 
-def main():
+def robust_test():
     if len(sys.argv) != 3:
         print("Usage: python main.py <numerator> <denominator>")
         sys.exit(1)
