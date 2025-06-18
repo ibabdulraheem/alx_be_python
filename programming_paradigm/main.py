dc8ca8a845f866fae7172a2330deb45546b658ee
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-06-18 22:50:09 +0100

    robust division

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -9,7 +9,7 @@ def main():
     numerator = sys.argv[1]
     denominator = sys.argv[2]
 
-    result = safe_divide(numerator, denominator)
+    result = safe_divide()
     print(result)
 
 if __name__ == "__main__":
