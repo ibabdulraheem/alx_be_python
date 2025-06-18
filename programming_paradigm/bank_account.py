7dfed84ee9a6220e1d1ea0966e7d2169a035045a
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-06-18 17:44:11 +0100

    bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -9,13 +9,13 @@ class BankAccount:
     return(f"Deposited: ${amount} ")
   def withdraw(self,amount):
     self.amount = amount
-    if self.account_balance == 0:
-       return(f"Your Account is zero")
-    elif self.account_balance < self.amount:
-      return("Insufficient funds.")
-    else:
+    if self.account_balance == 0 or self.account_balance < self.amount:
+       return("Insufficient funds.")
+    elif self.account_balance >= self.amount:
       self.account_balance -= self.amount
       return(f"withdrew: ${self.amount}")
+    else:
+      return("Invalid Transaction!")
   def display_balance(self):
       return(f"Current Balance: ${self.account_balance} ")
 
