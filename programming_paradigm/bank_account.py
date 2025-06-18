653a5ecf46eb9599cfd5f2f20c525fbae7ae8b65
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-06-18 17:22:28 +0100

    bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,14 +1,14 @@
 class BankAccount:
-  def __init__(self,account_balance,amount):
+  def __init__(self,account_balance):
     self.account_balance = account_balance
-    self.amount = amount
+
 
   def deposit(self,amount):
     self.amount = amount
     self.account_balance += self.amount
     return(f"Deposited: ${amount} ")
   def withdraw(self,amount):
-    self.amout = amount
+    self.amount = amount
     if self.account_balance == 0:
        return(f"Your Account is zero")
     elif self.account_balance < self.amount:
@@ -18,4 +18,4 @@ class BankAccount:
       return(f"withdrew: ${self.amount}")
   def display_balance(self):
       return(f"Current Balance: ${self.account_balance} ")
-account = BankAccount(100,50)
+
