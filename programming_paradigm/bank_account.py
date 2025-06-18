24616c9c619848ac1e8904a32709e38b899279c9
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-06-18 10:05:38 +0100

    bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -9,20 +9,22 @@ class BankAccount:
   
   def withdraw(self,amount):
     self.amount = amount
-    if self.account_balance > 0:
-       self.account_balance-= self.amount
-       print(f"Withdrew: ${self.amount}")
-    elif self.account_balance == 0:
-      print ("Insufficien account balance.")
+    if self.account_balance == 0:
+       print(f"Your Account is: {self.account_balance}")
+    elif self.account_balance < self.amount:
+      print ("Insufficien funds.")
     elif self.account_balance >= self.amount:
       self.account_balance -= self.amount
-      return (f"withdrew:, ${float(self.account_balance)}")
-  
+      print(f"withdrew:, ${float(self.account_balance)}")
   def display_balance(self):
-    print( f"Current Balance: ${float(self.account_balance)}" )
+      print( f"Current Balance: ${float(self.account_balance)}" )
 
 my_account = BankAccount(250)
 print(my_account.display_balance())
+my_account.deposit(50)
+my_account.display_balance()
+my_account.withdraw(2000)
+my_account.display_balance()
 
 
 
