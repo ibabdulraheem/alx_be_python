114d5937bb7dc7bae0c900d4784461ebe10c1744
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-06-19 22:02:03 +0100

    robust division

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,4 +1,6 @@
 def safe_divide(numerator,denominator):
+  numerator = float(input("Enter the Numerator: "))
+  denominator = float(input("Enter the denominator: "))
   result = numerator/denominator
   try:
       if denominator == 0:
