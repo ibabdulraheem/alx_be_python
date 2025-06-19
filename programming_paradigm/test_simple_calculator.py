e25d25e3c79a95b43451e4275bc33a28de043c19
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-06-19 22:59:56 +0100

    simple calculator test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -3,19 +3,19 @@ from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator (unittest.TestCase):
   def test_addition(self):
-    result = SimpleCalculator.add(self,10,13)
+    result = SimpleCalculator.add(10,13)
     self.assertEqual(result,23)
 
   def test_sub(self):
-    result = SimpleCalculator.subtract(self,10,5)
+    result = SimpleCalculator.subtract(10,5)
     self.assertEqual(result,5)
 
   def test_mult(self):
-    result = SimpleCalculator.multiply(self,10,5)
+    result = SimpleCalculator.multiply(10,5)
     self.assertEqual(result,50)
 
   def test_div(self):
-    result = SimpleCalculator.divide(self,10,5)
+    result = SimpleCalculator.divide(10,5)
     self.assertEqual(result,2)
     
 
