c81e6ea12457c83ec8cd33bff904af33800e5c43
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-06-19 12:32:45 +0100

    bank account

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,6 +1,6 @@
 def safe_divide (numerator,denominator ):
   try:
-      result = numerator / denominator
+      result = float(numerator / denominator)
       if denominator == 0:
         raise ZeroDivisionError ("Error: Cannot divide by zero.")
       else:
