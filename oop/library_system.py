31f6f3d1d10104ea791931717baed6dab27863d0
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-06-22 00:53:07 +0100

    library system

---
+++ oop/library_system.py
@@ -0,0 +1,25 @@
+class Book:
+  def __init__(self,title = str,author = str):
+    self.title = title
+    self.author = author
+
+class EBook(Book):
+  def __init__(self, title=str, author=str, file_size = int):
+    self.file_size = file_size
+    super().__init__(title, author)
+
+class PrintBook(Book):
+  def __init__(self, title=str, author=str,page_count = int):
+    self.page_count = page_count
+    super().__init__(title, author)
+
+class Library:  # Implementing composition
+  def __init__(self,books = []):
+    self.books = books
+  def add_book(self,book):
+    self.book = book
+    self.books.append(book)
+    print(self.books)
+
+my_book1 = Library("world power")
+my_book1.add_book("good")
