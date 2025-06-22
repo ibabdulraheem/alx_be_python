51b4ba9eadf46bf4587a313178875f63c0ddeeae
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-06-22 17:07:33 +0100

    calcuation of areas of different shapes

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,22 @@
+import math
+class Shape:
+  def __init__(self):
+    pass
+  def area(self):
+    raise NotImplementedError ("derived classes need to override this method.")
+
+class Rectangle(Shape):
+  def __init__(self,length,width):
+    self.length = length
+    self.width = width
+    super().__init__()
+  def area(self):
+    self.area = self.length * self.width
+    return self.area
+    
+class Circle(Shape):
+  def __init__(self,radius):
+    self.radius = radius
+    super().__init__()
+  def area(self):
+    return  math.pi * (self.radius**2)
