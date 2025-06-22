4955888c5ea7e1187def24cf9626e84a9d3dc5fa
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-06-22 17:42:50 +0100

    calcuation of areas of different shapes

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -19,6 +19,6 @@ class Circle(Shape):
     self.radius = radius
     super().__init__()
   def area(self):
-    self.radius_square = self.radius**2
-    return self.radius_square * math.pi
+    return(math.pi * self.radius**2)
+    
     
