5cb02baffb2d165ac48eebd6dbd24c7bca018c05
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-06-22 21:47:41 +0100

    class and static methods

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -5,10 +5,10 @@ class Calculator:
   def add (a ,b):
     return a + b
   
-  @classmethod
-  def multiply(cls, a,b ) :
-    print(f"Calculation type: {cls.calculation_type}")
-    return a * b
+@classmethod
+def multiply(cls,a,b):
+  print(f"Calculation type: {cls.calculation_type}")
+  return a * b
 
 
 
